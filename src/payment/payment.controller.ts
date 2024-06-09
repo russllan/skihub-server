0b0e9dbec241c02544b257afc03fb421fe50d720
russllan <rahmatov.ruslan.02@bk.ru>
2024-06-09 14:29:55 +0600

    feat: admin get for payment

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth('JWT-auth')
+@ApiTags('payment')
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
@@ -16,9 +19,17 @@ export class PaymentController {
     return this.paymentService.getAllPayments();
   }
 
+  @Get('adminGet/:id')
+  @UseGuards(JwtAuthGuard)
+  async getAdmin(@Param('id') id: string) {
+    return this.paymentService.getAdmin(+id);
+  }
+
   @Get('getOne')
   @UseGuards(JwtAuthGuard)
   async getOnePayment(@Req() req) {
     return this.paymentService.getOnePayment(+req.user.id);
   }
+
+ 
 }
